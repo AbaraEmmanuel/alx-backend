114cfd6be9cd1a72276eb86207733ca05bee51c1
Abara Emmanuel <emmanuelabara265@gmail.com>
2024-06-26 19:19:55 +0000

    Adding solution of task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,43 +1,68 @@
+
 #!/usr/bin/env python3
-""" BaseCaching module
+"""Create a class MRUCache that inherits
+from BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary
+from the parent class BaseCaching
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the most recently used item
+(MRU algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
 """
-from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """_summary_
     """
-    FIFOCache defines a FIFO caching system
-    """
+
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
+        """_summary_
         """
         super().__init__()
-        self.usage = []
+        self.usedKeys = []
 
     def put(self, key, item):
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
-        Cache a key-value pair
-        """
-        if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[0]))
-                del self.cache_data[self.usage[0]]
-                del self.usage[0]
-            if key in self.usage:
-                del self.usage[self.usage.index(key)]
-            self.usage.append(key)
+        if key is not None and item is not None:
             self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
         """
         if key is not None and key in self.cache_data.keys():
-            del self.usage[self.usage.index(key)]
-            self.usage.append(key)
-            return self.cache_data[key]
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
         return None
